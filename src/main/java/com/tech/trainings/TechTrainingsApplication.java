0415042b4c3d54fd799007987fe84b39bde96ff8
Akula Sankaraiah <akulasankaraiah9@gmail.com>
2020-10-17 17:28:42 +0800

    Initial commit

---
+++ src/main/java/com/tech/trainings/TechTrainingsApplication.java
@@ -0,0 +1,13 @@
+package com.tech.trainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrainingsApplication.class, args);
+	}
+
+}
